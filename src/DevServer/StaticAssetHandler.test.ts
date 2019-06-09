1c801f608c2ddc5c24c3ad994bd6f5c43f6034fd
Thanik Bhongbhibhat <email@thanik.me>
2019-06-09 11:33:18 -0700

    Update tests: StaticAssetHandler

--- src/DevServer/StaticAssetHandler.test.ts
+++ src/DevServer/StaticAssetHandler.test.ts
@@ -1,22 +1,29 @@
 jest.mock('./StaticAssetAdapter');
 
-// import { streamToString } from '../scrapbook/StreamUtils';
+import { streamToString } from '../scrapbook/StreamUtils';
 import { handler } from './StaticAssetHandler';
 
 describe('StaticAssetHandler', () => {
   describe('request existing files', () => {
     it('returns Task<FileExisted> response', async () => {
+      expect.assertions(2);
       const filename = 'should-find.txt';
-      const result = await handler({ filename }).run();
-      expect(result.statusCode).toBe(200);
+      const res = await handler({ filename }).run();
+      expect(res.statusCode).toBe(200);
+      expect(await streamToString(res.body)).toBe(
+        'fakeContent from should-find.txt'
+      );
     });
   });
 
   describe('request non-existing files', () => {
-    it('returns Task<FileNotFound> response',async () => {
+    it('returns Task<FileNotFound> response', async () => {
+      expect.assertions(2);
       const filename = 'should-not-find-this-file.abc';
-      const result = await handler({ filename }).run();
-      expect(result.statusCode).toBe(404);
+      const res = await handler({ filename }).run();
+      expect(res.statusCode).toBe(404);
+      // temporary
+      expect(await streamToString(res.body)).toBe('file not found');
     });
   });
 });
